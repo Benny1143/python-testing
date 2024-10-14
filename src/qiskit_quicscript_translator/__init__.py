a79a33b723f6a3f9a1e9f78a39cb06ba724fa5e8
benny <bennyrobot1143@gmail.com>
2024-10-14 15:23:33 +0800

    add translator

--- src/qiskit_quicscript_translator/__init__.py
+++ src/qiskit_quicscript_translator/__init__.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: 2024-present benny <bennyrobot1143@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-from qiskit_quicscript_translator.work import addh
+from qiskit_quicscript_translator.Translator import get_quic_circuit_string, get_quic_circuit_file_string
--- src/qiskit_quicscript_translator/work.py
+++
@@ -1,2 +0,0 @@
-def addh(a,b):
-    return a+b
